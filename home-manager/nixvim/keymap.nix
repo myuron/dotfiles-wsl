ae7b5ebc66f6ab8346555c93be3c463f2794b66d
myuron <myuron.dev@gmail.com>
2026-05-13 00:27:25 +0900

    chore(nixvim): add keymaps

--- home-manager/nixvim/keymap.nix
+++ home-manager/nixvim/keymap.nix
@@ -10,5 +10,56 @@
       key = "<ESC><ESC>";
       action = "<cmd>nohlsearch<CR>";
     }
+    # --------------------------------------------------
+    # Window Management
+    # --------------------------------------------------
+    {
+      mode = "n";
+      key = "ss";
+      action = "<cmd>split<CR>";
+    }
+    {
+      mode = "n";
+      key = "sv";
+      action = "<cmd>vsplit<CR>";
+    }
+    {
+      mode = "n";
+      key = "st";
+      action = "<cmd>terminal<CR>";
+    }
+    {
+      mode = "n";
+      key = "sh";
+      action = "<C-w>h";
+    }
+    {
+      mode = "n";
+      key = "sj";
+      action = "<C-w>j";
+    }
+    {
+      mode = "n";
+      key = "sk";
+      action = "<C-w>k";
+    }
+    {
+      mode = "n";
+      key = "sl";
+      action = "<C-w>l";
+    }
+    # --------------------------------------------------
+    # Buffer Management
+    # --------------------------------------------------
+    {
+      mode = "n";
+      key = "<Tab>";
+      action = "<cmd>bnext<CR>";
+    }
+    {
+      mode = "n";
+      key = "<S-Tab>";
+      action = "<cmd>bprev<CR>";
+    }
   ];
 }
