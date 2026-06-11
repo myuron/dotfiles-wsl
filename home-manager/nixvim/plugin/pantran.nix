760c2389b89f8199ff918ebc1dfba16945e28cd8
myuron <myuron.dev@gmail.com>
2026-06-11 01:27:18 +0000

    chore(nixvim): remove unnecessaary comments

--- home-manager/nixvim/plugin/pantran.nix
+++ home-manager/nixvim/plugin/pantran.nix
@@ -14,12 +14,6 @@ in
   extraPlugins = [(pkgs.vimUtils.buildVimPlugin {
     name = "pantran";
     src = sources.pantran.src;
-    # src = pkgs.fetchFromGitHub {
-    #   owner = "potamides";
-    #   repo = "pantran.nvim";
-    #   rev = "b87c3ae";
-    #   hash = "sha256-b4odpXwh+BmFsK5v3HmSWG43FA+ygOAPU+qFNy6vWDU=";
-    # };
   })];
   extraConfigLua = ''
     require("pantran").setup({
