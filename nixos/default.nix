ac823a0089edce4ba872d1359bbb94ccc8fe10cb
myuron <myuron.dev@gmail.com>
2026-06-18 12:34:45 +0000

    refactor

---
+++ nixos/default.nix
@@ -0,0 +1,40 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  wsl = {
+    enable = true;
+    defaultUser = "myuron";
+  };
+
+  networking.resolvconf.enable = false;
+
+  virtualisation.docker.enable = true;
+
+  programs = {
+    fish.enable = true;
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+    };
+  };
+
+  users.users.myuron = {
+    shell = pkgs.fish;
+    extraGroups = [ "docker" ];
+  };
+
+  system.stateVersion = "25.11";
+
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    allow-import-from-derivation = false;
+    sandbox = true;
+  };
+}
