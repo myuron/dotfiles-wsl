5caf5f990e3a2633c0798f6edb0317cee67db783
myuron <myuron.dev@gmail.com>
2026-06-22 08:31:39 +0000

    feat(emacs): org-mode

--- home-manager/emacs/init.el
+++ home-manager/emacs/init.el
@@ -52,7 +52,14 @@
 ;; org-mode
 (leaf org
   :custom
-  (org-directory . "~/org"))
+  (org-directory . "~/org")
+  (org-agenda-files . '("~/org"))
+  (org-refile-targets . '((org-agenda-files :maxlevel . 1)))
+  (org-columns-default-format . "%68ITEM(Task) %6Effort(Effort){:} %6CLOCKSUM(Clock){:}")
+  (org-startup-indented . t)
+  :bind (("C-c t i" . org-clock-in)
+         ("C-c t o" . org-clock-out)
+	 ("C-c t s" . org-set-effort)))
 
 ;; ナレッジ管理
 (leaf org-roam
@@ -66,17 +73,17 @@
 
 ;; タスク管理
 (leaf org-agenda
-  :custom
-  (org-agenda-files . `(,org-directory))
-  (org-agenda-window-setup . current-window)
+  :require t
+  :setq ((org-agenda-custom-commands quote
+				     (("x" "Unscheduled Tasks" tags-todo "-SCHEDULED>=\"<today>\"-DEADLINE>=\"<today>\"" nil))))
   :bind (("C-c a" . org-agenda)))
 
 (leaf org-capture
   :custom
-  (org-capture-templates . '(("t" "Todo" entry (file+headline "~/org/0_todo.org" "Unorganized")
+  (org-capture-templates . '(("t" "Todo Entroy to Inbox" entry (file+headline "~/org/0_inbox.org" "Inbox")
 			      "** TODO %?\n")))
   :bind (("C-c c" . org-capture)))
 
 ;; タイム管理
 (leaf org-pomodoro
-  :bind (("C-c p" . org-pomodoro)))
+  :bind (("C-c p" . org-pomodoro))) 
